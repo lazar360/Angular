f32e2b814347530ab948b87173a182d1557adc95
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-09-26 22:59:29 +0200

    add address to Parkinginfo

--- src/app/parkings/parkings.component.ts
+++ src/app/parkings/parkings.component.ts
@@ -9,12 +9,23 @@ import { ParkingService } from '../services/parking.service';
 })
 export class ParkingsComponent implements OnInit {
   parkings: Parkinginfo[] = [];
+  parkingsAddressTmp:any[]=[];
   isLoaded: boolean = false;
 
   constructor(private parkingService: ParkingService) {}
 
   ngOnInit(): void {
+    this.parkingService.getAddressParkings().subscribe((res) => {
+       console.log(res);
+      this.parkingsAddressTmp = res;
+    });
+    
     this.parkingService.getParkings().subscribe((res) => {
+      res.forEach(park => {
+        this.parkingsAddressTmp.forEach(address => {
+          park.adresse = park.id === address.id ? address.adresse : park.adresse;   
+        })
+      })
       this.parkings = res;
       this.isLoaded = true;
     });
--- src/app/services/parking.service.ts
+++ src/app/services/parking.service.ts
@@ -8,23 +8,41 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class ParkingService {
-  private url =
-    'https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_parkings-publics-nantes-disponibilites/records?limit=20';
+  private urlPlacesDispo =
+    'https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_parkings-publics-nantes-disponibilites/records?limit=100';
+
+  private urlAddress =
+    'https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_parkings-publics-nantes/records?limit=100';
 
   constructor(private httpClient: HttpClient) {}
 
   getParkings(): Observable<Parkinginfo[]> {
     return this.httpClient
-      .get<any>(this.url) // Use "any" here to handle the API response
+      .get<any>(this.urlPlacesDispo) // Use "any" here to handle the API response
       .pipe(
         map((response) =>
           response.results.map((parkingData: any) => {
-            console.log(parkingData);
+            // console.log(parkingData);
             return {
               identifiant: parkingData.idobj,
               nom: parkingData.grp_nom,
-              adresse: 'longitude : ' + parkingData.location.lon + ' - latitude : ' +  parkingData.location.lat,
               nbPlacesVoiture: parkingData.grp_disponible,
+            };
+          })
+        )
+      );
+  }
+
+  getAddressParkings(): Observable<Parkinginfo[]> {
+    return this.httpClient
+      .get<any>(this.urlAddress) // Use "any" here to handle the API response
+      .pipe(
+        map((response) =>
+          response.results.map((parkingData: any) => {
+            // console.log(parkingData);
+            return {
+              identifiant: parkingData.idobj,
+              adresse: parkingData.adresse + ' - ' + parkingData.commune
             };
           })
         )
